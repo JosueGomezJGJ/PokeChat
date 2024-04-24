4e347112cc49ace4bd4d17b06919791f736709e5
JosueGomezJGJ <99923275+JosueGomezJGJ@users.noreply.github.com>
2024-04-24 23:23:19 +0000

    added ability to play pokemon cries

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { Card, Image, List, ListItem, Button } from "semantic-ui-react";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Card,
+  Image,
+  List,
+  ListItem,
+  Button,
+  Icon,
+  Label,
+} from "semantic-ui-react";
 import axios from "axios";
+
+const typeColors = {
+  normal: "grey",
+  fire: "red",
+  water: "blue",
+  electric: "yellow",
+  grass: "green",
+  ice: "teal",
+  fighting: "orange",
+  poison: "purple",
+  ground: "brown",
+  flying: "violet",
+  psychic: "pink",
+  bug: "olive",
+  rock: "brown",
+  ghost: "violet",
+  dragon: "blue",
+  dark: "black",
+  steel: "grey",
+  fairy: "pink",
+};
 
 const PokemonCard = ({ pokemonID }) => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
   const [currentImage, setCurrentImage] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const audioRef = useRef(new Audio());
 
   useEffect(() => {
-    if (data[pokemonID]) {
-      console.log(data);
-      return;
-    }
-
     setLoading(true);
-
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
       .then((response) => {
@@ -30,38 +55,36 @@ const PokemonCard = ({ pokemonID }) => {
       });
   }, [pokemonID]);
 
-  const pokemonData = data[pokemonID];
+  useEffect(() => {
+    if (data[pokemonID]) {
+      audioRef.current.src = data[pokemonID].cries.latest;
+      audioRef.current.volume = 0.5;
+      audioRef.current.addEventListener("ended", handleAudioEnded);
+    }
 
-  if (!pokemonData) {
-    return loading ? <div>Loading...</div> : <div>No data available</div>;
-  }
+    return () => {
+      audioRef.current.removeEventListener("ended", handleAudioEnded);
+    };
+  }, [data, pokemonID]);
 
-  const typeColors = {
-    normal: "grey",
-    fire: "red",
-    water: "blue",
-    electric: "yellow",
-    grass: "green",
-    ice: "teal",
-    fighting: "orange",
-    poison: "purple",
-    ground: "brown",
-    flying: "violet",
-    psychic: "pink",
-    bug: "olive",
-    rock: "brown",
-    ghost: "violet",
-    dragon: "blue",
-    dark: "black",
-    steel: "grey",
-    fairy: "pink",
+  const togglePlay = () => {
+    if (playing) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setPlaying(!playing);
+  };
+
+  const handleAudioEnded = () => {
+    setPlaying(false);
   };
 
   const images = [
-    pokemonData.sprites.front_default,
-    pokemonData.sprites.back_default,
-    pokemonData.sprites.front_shiny,
-    pokemonData.sprites.back_shiny,
+    data[pokemonID]?.sprites.front_default,
+    data[pokemonID]?.sprites.back_default,
+    data[pokemonID]?.sprites.front_shiny,
+    data[pokemonID]?.sprites.back_shiny,
   ].filter(Boolean);
 
   const handleNext = () => {
@@ -72,24 +95,43 @@ const PokemonCard = ({ pokemonID }) => {
     setCurrentImage((currentImage - 1 + images.length) % images.length);
   };
 
+  if (!data[pokemonID]) {
+    return loading ? <div>Loading...</div> : <div>No data available</div>;
+  }
+
   return (
     <Card>
-      <Image src={images[currentImage]} alt="Pokemon Sprite" size="large" />
-      <Button.Group>
-        <Button onClick={handlePrevious} icon="left chevron" />
-        <Button onClick={handleNext} icon="right chevron" />
+      <Image
+        src={images[currentImage]}
+        alt="Pokemon Sprite"
+        wrapped
+        ui={false}
+      />
+      <Button.Group attached="top" widths="2">
+        <Button icon onClick={handlePrevious}>
+          <Icon name="left chevron" />
+        </Button>
+        <Button icon onClick={handleNext}>
+          <Icon name="right chevron" />
+        </Button>
       </Button.Group>
       <Card.Content>
         <Card.Header>
-          {pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}
+          {data[pokemonID].name.charAt(0).toUpperCase() +
+            data[pokemonID].name.slice(1)}
+          <Button icon floated="right" onClick={togglePlay}>
+            <Icon name={playing ? "pause" : "play"} />
+          </Button>
         </Card.Header>
-        {pokemonData.types.map((type, index) => (
-          <div key={index} className={`ui ${typeColors[type.type.name]} label`}>
-            {type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}
-          </div>
-        ))}
-        <List divided relaxed className="large">
-          {pokemonData.stats.map((stat, index) => (
+        <Card.Meta>
+          {data[pokemonID].types.map((type, index) => (
+            <Label key={index} color={typeColors[type.type.name]}>
+              {type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}
+            </Label>
+          ))}
+        </Card.Meta>
+        <List divided relaxed>
+          {data[pokemonID].stats.map((stat, index) => (
             <ListItem key={index}>
               <List.Content floated="left">
                 {stat.stat.name.toUpperCase()}
